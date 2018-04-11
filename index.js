b1f03f5144ceab32c8dffc7b33a5c420b0045487
Ruba <ruba.noureldin@namshi.com>
2018-04-11 18:22:45 +0400

    combine format functions

--- index.js
+++ index.js
@@ -15,42 +15,28 @@ const isError = e => (
   e instanceof Error || (e && e.stack && e.message)
 );
 
-
-/**
- * If the arg is an Error object, we need only to extract message, stack and statusCode. message is appended to the log message with a prefix 'Error: '. 
- * If not an error, extend the original objToLog with all arg properties
- * @param {*} objToLog object contains all properties to log
- * @param {*} arg JSON object argument
- */
-const formatObject = (objToLog, arg) => {
-  if (!arg) {
-    return objToLog;
-  }
-
-  return Object.assign(
-    {},
-    objToLog,
-    !isError(arg) ? arg : {
-      status: arg.statusCode || 500,
-      stack: arg.stack,
-      message: [...objToLog.message, `Error: ${arg.message}`],
-    },
-  );
-};
-
 /**
  * Parse the list arguments passed to be be logged and return a single JSON object
+ * If the arg is an Error object, we need only to extract message, stack and statusCode. message is appended to the log message with a prefix 'Error: '
+ * If not an error, extend the original objToLog with all arg properties
  * If an argument is a primitive or an array, append it to the log message after converting to a string.
- * If it's an object, delegate to formatObject function to handle it
  * @param {*} args The passed parameters to be logged
  * @return objToLog: JSON object contains all parameters to be sent to winston context including a message string
- * TODO: run async?
+ * TODO: use async?
  */
 const parseArgs = (args) => {
   let objToLog = { message: [] };
   args.forEach((arg) => {
-    if (typeof arg === 'object' && !Array.isArray(arg)) {
-      objToLog = formatObject(objToLog, arg);
+    if (!arg) {
+      return;
+    }
+
+    if (isError(arg)) {
+      objToLog.status = arg.statusCode || 500;
+      objToLog.stack = arg.stack;
+      objToLog.message.push(util.inspect(arg));
+    } else if (typeof arg === 'object' && !Array.isArray(arg)) {
+      objToLog = Object.assign({}, objToLog, arg); 
     } else {
       objToLog.message.push(util.inspect(arg));
     }
