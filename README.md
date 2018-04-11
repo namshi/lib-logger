03e3e5850fb4d0ec56b688028d49da486de6fd94
Ruba <ruba.noureldin@namshi.com>
2018-04-11 14:52:12 +0400

    Support multi params logging

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Lib-logger
 
-Library that simplifies the way we log.
+Library that simplifies the way we log. It eases migration from native console.log to other log libraries. Current release wrapped winston logger but more loggers can be supported later.
 
 
 ## Features
@@ -19,3 +19,20 @@ Install it through: `npm i lib-logger`
     
     log.debug("This is a debug message!");
     log.info("This is an info message");
+
+# Migration From console.log to lib-logger
+To ease migrating from console.log to lib-logger, the library supports receiving a number of arguments (should be limited) that are not wrapped in a json object. The library will parse and format it as follows:
+- If an argument is a primitive or an Array, convert it to a string and append it to the log message separated with a ', '
+- If the argument is an Error, we append the error message to the log message with a prefix ' Error: ' and add stack and statusCode to the context json object. 
+- If the argument is a JSON object, we append all its properties to the context object.
+
+example: log.error('Shipment Number', 1234, "could not be created", new Error('Error while bla bla'), { name: 'Test Name', locale: 'en', country: 'ae'});
+Output:  
+    message: 'Shipment Number, 1234, could not be created, Error: Error while bla bla' it's
+    context object: {
+        stack: "Error while bla bla @FileName ..... stacktrace',
+        statusCode: 500, // default value if no code provided in the error object
+        name: 'Test Name',
+        locale: 'en',
+        country: 'ae',
+    }
--- index.js
+++ index.js
@@ -1,59 +1,78 @@
-const winston = require('winston');
-const { combine, timestamp } = winston.format;
+const util = require('util');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp } = format;
 
-const log = winston.createLogger({
+const log = createLogger({
   level: 'info',
-  format: combine(timestamp(), winston.format.json()),
-  transports: [new winston.transports.Console()],
+  format: combine(
+    timestamp(),
+    format.json(),
+  ),
+  transports: [new transports.Console({ json: true })],
 });
 
-const isError = function(e) {
-  return (
-    e instanceof Error ||
-    (e &&
-      e.stack &&
-      e.message &&
-      typeof e.stack === 'string' &&
-      typeof e.message === 'string')
+const isError = e => (
+  e instanceof Error || (e && e.stack && e.message)
+);
+
+
+/**
+ * If the arg is an Error object, we need only to extract message, stack and statusCode. message is appended to the log message with a prefix 'Error: '. 
+ * If not an error, extend the original objToLog with all arg properties
+ * @param {*} objToLog object contains all properties to log
+ * @param {*} arg JSON object argument
+ */
+const formatObject = (objToLog, arg) => {
+  if (!arg) {
+    return objToLog;
+  }
+
+  return Object.assign(
+    {},
+    objToLog,
+    !isError(arg) ? arg : {
+      status: arg.statusCode || 500,
+      stack: arg.stack,
+      message: [...objToLog.message, `Error: ${arg.message}`],
+    },
   );
 };
 
-const logger = {
-  warn: (...args) => {
-    log.warn(...args);
-  },
-  error: (...args) => {
-    log.error(...args);
-  },
-  debug: (...args) => {
-    log.debug(...args);
-  },
-  info: (...args) => {
-    log.info(...args);
-  },
-  level: level => {
-    for (let key in log.transports) {
-      log.transports[key].level = level;
+/**
+ * Parse the list arguments passed to be be logged and return a single JSON object
+ * If an argument is a primitive or an array, append it to the log message after converting to a string.
+ * If it's an object, delegate to formatObject function to handle it
+ * @param {*} args The passed parameters to be logged
+ * @return objToLog: JSON object contains all parameters to be sent to winston context including a message string
+ * TODO: run async?
+ */
+const parseArgs = (args) => {
+  let objToLog = { message: [] };
+  args.forEach((arg) => {
+    if (typeof arg === 'object' && !Array.isArray(arg)) {
+      objToLog = formatObject(objToLog, arg);
+    } else {
+      objToLog.message.push(util.inspect(arg));
     }
-  },
-  log: (obj, context) => {
-    let error = isError(obj);
-    context.status = context.status || 500;
-    let level =
-      !error && (!context.status || context.status < 400)
-        ? 'info'
-        : !error && context.status < 500 ? 'warn' : 'error';
-    if (error) {
-      if (obj.statusCode) {
-        context.status = error.statusCode;
-      }
-      if (obj.stack) {
-        context.stack = obj.stack;
-      }
-      obj = obj.message;
-    }
-    logger[level](obj, context);
-  },
+  });
+
+  objToLog.message = objToLog.message.join(', ');
+  return objToLog;
+};
+
+// define Logger
+const logger = {};
+Object.keys(log.levels).forEach((level) => {
+  logger[level] = (...args) => {
+    const obj = parseArgs(args);
+    log[level](obj.message, obj);
+  };
+});
+
+logger.setLevel = (level) => {
+  log.transports.forEach((transport) => {
+    transport.level = level;
+  });
 };
 
 module.exports = { logger };
