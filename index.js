2d518eb6835e423d59afbf5032a913d35794568f
Carles <carles.sanchez@namshi.com>
2019-05-08 23:00:10 +0400

    Added ci inside package.json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const util = require("util");
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp } = format;
+const { either } = require("keyu");
 
 const log = createLogger({
   level: "info",
@@ -10,7 +11,8 @@ const log = createLogger({
 
 const isError = e => e instanceof Error || (e && e.stack && e.message && true) || false;
 const isObject = obj => typeof obj === "object" && !Array.isArray(obj) && obj !== null;
-const dataStringify = data => (isError(data) ? util.inspect(data) : JSON.stringify(data)).replace(/\n|\t|\r/g, "");
+const stringifyOr = either(data => (isError(data) ? util.inspect(data) : JSON.stringify(data)), data => `${data}`);
+const dataStringify = data => stringifyOr(data).replace(/\n|\t|\r/g, "");
 const addMessage = (arg, { messages = [] } = {}) => (arg || arg === 0 ? messages.concat(dataStringify(arg)) : messages);
 
 const addArg = (data, res = { context: {}, messages: [] }) => (isError(data) && withError(data, res)) || (isObject(data) && withObject(data, res)) || withMessages(data, res);
@@ -27,16 +29,6 @@ const withError = (err, res = { context: {}, messages: [] }) => {
 };
 const withObject = (obj, res = { context: {}, messages: [] }) => (obj ? { ...res, context: Object.assign({}, res.context, obj), messages: res.messages || [] } : res);
 const withMessages = (obj, res = { context: {}, messages: [] }) => Object.assign({}, res, { messages: addMessage(obj, res) });
-/**
- * Parse passed arguments list and return a single JSON object to be be logged.
- * If the arg is an Error object, we need only to extract message, stack and statusCode.
- * message is appended to the log message with a prefix 'Error: '
- * If not an error, extend the original objToLog with all arg properties
- * If an argument is a primitive or an array, append it to the log message after converting to a string.
- * @param {*} args The passed parameters list to be logged
- * @return objToLog: JSON object contains all parameters to be sent to winston context including a message string
- * TODO: make it async?
- */
 
 const parseArgs = args => {
   const obj = args.reduce((acc, data) => addArg(data, acc), { context: {}, messages: [] });
@@ -53,9 +45,7 @@ const logger = Object.keys(log.levels).reduce((acc, val) => {
 }, {});
 
 logger.setLevel = level => {
-  log.transports.forEach(transport => {
-    transport.level = level;
-  });
+  log.transports = log.transports.map(transport => ({ ...transport, level }));
 };
 
 module.exports = logger;
